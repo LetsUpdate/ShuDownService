5890dad21ec43d88f3f3bb80eb88fe358a630b7a
RED <tancros.janos@gmail.com>
2019-07-29 16:28:41 +0200

    nackup point

--- src/main/java/hu/service/shutdown/InputSpringApplication.java
+++ src/main/java/hu/service/shutdown/InputSpringApplication.java
@@ -2,10 +2,10 @@ package hu.service.shutdown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class InputSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(InputSpringApplication.class, args);
     }
