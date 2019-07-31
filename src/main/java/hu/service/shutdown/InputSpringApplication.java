59b9c1d2bbe6b286d76db1f3e7cb23b0ac0f3573
RED <tancros.janos@gmail.com>
2019-07-31 14:45:13 +0200

    nackup point

--- src/main/java/hu/service/shutdown/InputSpringApplication.java
+++ src/main/java/hu/service/shutdown/InputSpringApplication.java
@@ -2,9 +2,8 @@ package hu.service.shutdown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class InputSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(InputSpringApplication.class, args);
--- src/main/java/hu/service/shutdown/config/SecurityConf.java
+++ src/main/java/hu/service/shutdown/config/SecurityConf.java
@@ -4,24 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-@Configuration
-@EnableWebSecurity
-public class SecurityConf extends WebSecurityConfigurerAdapter {
 
+@Configuration
+public class SecurityConf extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/webhook").permitAll()
-                .anyRequest().hasRole("USER");
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles("USER");
     }
 }
